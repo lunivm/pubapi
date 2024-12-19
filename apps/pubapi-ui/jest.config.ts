2b0c8c93b18cf2feae41b21ecfaf71b32eeb073d
Mykola Luniv <lunivm@yahoo.com>
2024-12-19 16:53:51 +0200

    rename

--- apps/pubapi-ui/jest.config.ts
+++ apps/pubapi-ui/jest.config.ts
@@ -1,8 +1,8 @@
 export default {
-  displayName: 'pubapi',
+  displayName: 'pubapi-ui',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/apps/pubapi',
+  coverageDirectory: '../../coverage/apps/pubapi-ui',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
