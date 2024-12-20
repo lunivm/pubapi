abb1bbe81c4f7604c8a3657c0b42a05dc56d88bf
Mykola Luniv <lunivm@yahoo.com>
2024-12-20 17:18:16 +0200

    add faker

--- apps/main-api/src/app/app.service.ts
+++ apps/main-api/src/app/app.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@nestjs/common';
-import * as dataMock from './users-data.mock.json'
+import { faker } from '@faker-js/faker';
+
+const seed = 13
+const collectionSize = 1000
 
 @Injectable()
 export class AppService {
+  private collection = [];
+
+  constructor() {
+    faker.seed(seed)
+    this.setCollection()
+  }
+
   getData(): {}[] {
-    return dataMock;
+    return this.collection;
+  }
+
+  private setCollection() {
+    for (let i = 0; i < collectionSize; i++) {
+      this.collection.push(this.getSong())
+    }
+  }
+
+  private getSong() {
+    const {
+      album,
+      artist,
+      genre,
+      songName
+    } = faker.music;
+
+    return {
+      album: album(),
+      artist: artist(),
+      genre: genre(),
+      songName: songName(),
+    }
   }
 }
